b66e223aece5ca6ea4661c112b4cb169c30868f0
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-08-14 19:05:01 +0530

    repel takes input and prints, exit's onpressing ctrl-c or ctrl-d

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello from c repel");
+    let mut rl = rustyline::DefaultEditor::new().expect("failed to start repel");
+
+    println!("crepel v0.1.0");
+
+    loop {
+        match rl.readline("> ") {
+            Ok(line) => {
+                println!("{}", line.trim());
+            }
+
+            Err(e) => {
+                match e {
+                    rustyline::error::ReadlineError::Eof => println!("CTRL-D"),
+                    rustyline::error::ReadlineError::Interrupted => println!("CTRL-C"),
+                    _ => {}
+                }
+                break;
+            }
+        }
+    }
 }
